91541186d324750c50452b03fd335bebd3fcfa40
Diego Alonso <alonmoratiel01@gmail.com>
2025-08-27 18:45:29 +0200

    first_commit

---
+++ src/lambda_function.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table("visitors-count-crc")
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return int(obj)  
+    raise TypeError
+
+def lambda_handler(event, context):
+    response = table.update_item(
+    Key={
+        'name': "visitors-count"
+    },
+    UpdateExpression="ADD nvisitors :increase",
+    ExpressionAttributeValues={
+        ':increase': 1
+    },
+    ReturnValues="UPDATED_NEW"
+    )
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response['Attributes'], default=decimal_default)
+    }
